cd540e6f755004e0f6a5f5970e6aff9ca57153b9
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2024-05-30 15:16:50 +0300

    Изменил InputManager и MoveController

--- Assets/Scripts/Components/MoveComponent.cs
+++ Assets/Scripts/Components/MoveComponent.cs
@@ -8,16 +8,22 @@ namespace ShootEmUp
         [SerializeField] private Rigidbody2D _rigidbody2D;
         [SerializeField] private float _speed = 5.0f;
 
-        private void FixedUpdate()
+        private void OnEnable()
         {
-            if (_inputManager != null)
-            {
-                float horizontalDirection = _inputManager.HorizontalDirection;
-                MoveByRigidbodyVelocity(new Vector2(horizontalDirection, 0) * Time.fixedDeltaTime);
-            }
+            _inputManager.OnMove += OnMove;
         }
 
-        public void MoveByRigidbodyVelocity(Vector2 vector)
+        private void OnDisable()
+        {
+            _inputManager.OnMove -= OnMove;
+        }
+
+        private void OnMove(float direction)
+        {
+            MoveByRigidbodyVelocity(new Vector2(direction, 0) * Time.fixedDeltaTime);
+        }
+
+        private void MoveByRigidbodyVelocity(Vector2 vector)
         {
             var nextPosition = this._rigidbody2D.position + vector * this._speed;
             this._rigidbody2D.MovePosition(nextPosition);
